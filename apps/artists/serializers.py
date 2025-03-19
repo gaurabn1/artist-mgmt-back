89ace1d691f60c57ecc26ea774e1e975672f43bf
gaurabn1 <gaurabneupane8969@gmail.com>
2025-03-19 22:58:06 +0545

    chore: refactor create operation for artist and user model

--- apps/artists/serializers.py
+++ apps/artists/serializers.py
@@ -4,11 +4,13 @@ from django.db import connection
 from rest_framework import serializers
 
 from apps.core.models import Artist, User
+from apps.core.utils import convert_tuples_to_dicts
 
 
 class ArtistSerializer(serializers.Serializer):
     uuid = serializers.UUIDField(read_only=True)
     name = serializers.CharField(required=True)
+    user_id = serializers.UUIDField(required=True)
     manager = serializers.PrimaryKeyRelatedField(
         queryset=User.objects.filter(role=User.Role.ARTIST_MANAGER),
         required=False,
@@ -16,7 +18,7 @@ class ArtistSerializer(serializers.Serializer):
     )
     manager_name = serializers.CharField(read_only=True)
     first_released_year = serializers.IntegerField(required=True)
-    no_of_album_released = serializers.IntegerField(required=True)
+    no_of_album_released = serializers.IntegerField(required=False, default=0)
     dob = serializers.DateField(required=True)
     gender = serializers.ChoiceField(required=True, choices=Artist.Gender.choices)
     address = serializers.CharField(required=True)
@@ -48,7 +50,42 @@ class ArtistSerializer(serializers.Serializer):
         return attrs
 
     def create(self, validated_data):
-        return Artist.objects.create(**validated_data)
+        # return Artist.objects.create(**validated_data)
+        with connection.cursor() as c:
+            c.execute(
+                """
+                INSERT INTO artists_artist
+                (name, first_released_year, no_of_album_released, dob, gender, address, manager_id, created_at, updated_at, user_id)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, NOW(), NOW(), %s)
+                RETURNING uuid, name, first_released_year, no_of_album_released, dob, gender, address, manager_id, user_id
+                """,
+                [
+                    validated_data.get("name", ""),
+                    validated_data.get("first_released_year", ""),
+                    validated_data.get("no_of_album_released", 0),
+                    validated_data.get("dob", ""),
+                    validated_data.get("gender", ""),
+                    validated_data.get("address", ""),
+                    validated_data.get("manager", None),
+                    validated_data.get("user_id", None),
+                ],
+            )
+            artist = c.fetchone()
+            artist = convert_tuples_to_dicts(
+                artist,
+                [
+                    "uuid",
+                    "name",
+                    "first_released_year",
+                    "no_of_album_released",
+                    "dob",
+                    "gender",
+                    "address",
+                    "manager_id",
+                    "user_id",
+                ],
+            )[0]
+            return artist
 
     def update(self, instance, validated_data):
         with connection.cursor() as c:
